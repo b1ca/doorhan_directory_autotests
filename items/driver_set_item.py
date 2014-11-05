4880c7cd53812d968bdd42a551bdcc122f1addc2
Anton Podobytko <bicaaaaa@gmail.com>
2014-11-05 17:59:16 +0300

    add new driver_sets tests

--- items/driver_set_item.py
+++ items/driver_set_item.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from simple_item import SimpleItem
 from helpers.waits import *
+from helpers.nomenclature_dialog import choose_random_element_from_dict
 
 
 class DriverSet(SimpleItem):
@@ -10,6 +11,9 @@ class DriverSet(SimpleItem):
     item_type = "driverSet"
     element_text = None
     product_mark = "DriverSetProduct"
+    dependent_element_text = None
+    product_count = None
+    group_name = 'RH58'
 
     def _update_element_params(self, params_list):
         self.add_product()
@@ -18,5 +22,41 @@ class DriverSet(SimpleItem):
     def update_element_params_without_information(self, params_list):
         for param in params_list:
             self.do_action(param)
-        self.driver.find_element_by_css_selector("#yt2").click()
+        self.save_element()
         wait_until_jquery(self, 5)
+
+    def save_element(self):
+        self.driver.find_element_by_xpath(
+            "//a[@id='yt4'][.='Сохранить'] | //a[@id='yt2'][.='Добавить'] | //a[@id='yt2'][.='Сохранить']").click()
+
+    def add_dependent_element(self, product_count, dep_el_params, as_group=None):
+        self.product_count = product_count
+        self.driver.find_element_by_css_selector(".products[count='%s'] a[id*='yt']" % product_count).click()
+        if as_group:
+            self.driver.find_element_by_css_selector("input[value='group']").click()
+            group_input = self.driver.find_element_by_css_selector("#autocompleteGroups")
+            group_input.click()
+            group_input.clear()
+            group_input.send_keys(self.group_name)
+            self.driver.find_element_by_xpath("//a[.='%s']" % self.group_name).click()
+            self.dependent_element_text = self.group_name
+        else:
+            self.driver.find_element_by_css_selector("a[onclick*='#dictionary-nomenclature']").click()
+            self.dependent_element_text = choose_random_element_from_dict(self)
+        for param in dep_el_params:
+            self.do_action(param)
+        self.save_dependent_element()
+
+    def save_dependent_element(self):
+        self.driver.find_element_by_css_selector("#yt0").click()
+
+    def to_update_dependent_element(self):
+        self.driver.find_element_by_css_selector("a[href*='editDependElement']").click()
+
+    def element_have_dependent(self, dependent_element_params):
+        assert self.dependent_element_text in self.driver.find_element_by_css_selector(
+            ".products[count='%s']" % self.product_count).text
+        self.to_update_dependent_element()
+        result = all(self.check_params_on_page(param) for param in dependent_element_params)
+        self.driver.find_element_by_css_selector(".btn-white a").click()
+        return result
--- tests/tests_driver_sets.py
+++ tests/tests_driver_sets.py
@@ -12,40 +12,40 @@ class TestsDriverSets(BaseTest):
         constructor_page = main_page.navigate_cp()
         self.item = constructor_page.navigate_driver_set()
 
-    def test01_add_driver_set(self):
+    def _add_simple_driver_set(self, params_list):
         dr_set = self.item
+        dr_set.add_element(params_list)
+        self.assertTrue(dr_set.have_element())
+        return dr_set
+
+    def test01_add_driver_set(self):
         params_list = [
             ["version", ["Внутренняя"]],
             ["region", ["СНГ"]],
         ]
-        dr_set.add_element(params_list)
-        self.assertTrue(dr_set.have_element())
+        dr_set = self._add_simple_driver_set(params_list)
         dr_set.to_update_element()
         self.assertTrue(dr_set.element_have_params(params_list))
         dr_set.delete_element()
         self.assertFalse(dr_set.have_element())
 
     def test02_add_driver_set(self):
-        dr_set = self.item
         params_list = [
             ["version", ["Дилерская"]],
             ["region", ["СНГ"]],
         ]
-        dr_set.add_element(params_list)
-        self.assertTrue(dr_set.have_element())
+        dr_set = self._add_simple_driver_set(params_list)
         dr_set.to_update_element()
         self.assertTrue(dr_set.element_have_params(params_list))
         dr_set.delete_element()
         self.assertFalse(dr_set.have_element())
 
     def test03_change_driver_set(self):
-        dr_set = self.item
         params_list = [
             ["version", ["Дилерская"]],
             ["region", ["СНГ"]],
         ]
-        dr_set.add_element(params_list)
-        self.assertTrue(dr_set.have_element())
+        dr_set = self._add_simple_driver_set(params_list)
         dr_set.to_update_element()
         new_params_list = [
             ["fx", ["По умолчанию:", "1"]],
@@ -54,5 +54,120 @@ class TestsDriverSets(BaseTest):
         self.assertTrue(dr_set.have_element())
         dr_set.to_update_element()
         self.assertTrue(dr_set.element_have_params(new_params_list))
+        dr_set.delete_element()
+        self.assertFalse(dr_set.have_element())
+
+    def test04_add_second_product(self):
+        params_list = [
+            ["version", ["Внутренняя"]],
+            ["region", ["СНГ"]],
+        ]
+        product_params = (1, 'RSD 01', ('Цех', 'Кладовщик', 'Мастер'))
+        dr_set = self._add_simple_driver_set(params_list)
+        dr_set.to_update_element()
+        dr_set.add_second_product(product_params)
+        dr_set._element_have_params(params_list)
+        dr_set.save_element()
+        self.assertTrue(dr_set.have_element())
+        dr_set.to_update_element()
+        self.assertTrue(dr_set.element_have_product(product_params))
+        self.assertTrue(dr_set.element_have_params(params_list))
+        dr_set.delete_element()
+        self.assertFalse(dr_set.have_element())
+
+    def _add_simple_driver_set_with_2_products(self, params_list, product_params):
+        dr_set = self._add_simple_driver_set(params_list)
+        dr_set.to_update_element()
+        dr_set.add_second_product(product_params)
+        dr_set._element_have_params(params_list)
+        dr_set.save_element()
+        return dr_set
+
+    def test05_add_dependent_element_to_first_product(self):
+        params_list = [
+            ["version", ["Внутренняя"]],
+            ["region", ["СНГ"]],
+        ]
+        product_params = (1, 'RSD 01', ('Цех', 'Кладовщик', 'Мастер'))
+        dr_set = self._add_simple_driver_set_with_2_products(params_list, product_params)
+        dependent_element_params = [
+            ["fx", ["Количество", "1"]],
+            ["version", ["Дилерская"]],
+            ["region", ["Азия", "Европа"]],
+        ]
+        dr_set.to_update_element()
+        dr_set.add_dependent_element(0, dependent_element_params)
+        dr_set.save_element()
+        dr_set.to_update_element()
+        self.assertTrue(dr_set.element_have_dependent(dependent_element_params))
+        self.assertTrue(dr_set.element_have_params(params_list))
+        dr_set.delete_element()
+        self.assertFalse(dr_set.have_element())
+
+    def test06_add_dependent_element_to_second_product_as_group_element(self):
+        params_list = [
+            ["version", ["Внутренняя"]],
+            ["region", ["СНГ"]],
+        ]
+        product_params = (1, 'RSD 01', ('Цех', 'Кладовщик', 'Мастер'))
+        dr_set = self._add_simple_driver_set_with_2_products(params_list, product_params)
+        dependent_element_params = [
+            ["fx", ["Количество", "1"]],
+            ["version", ["Дилерская"]],
+            ["region", ["Азия", "Европа"]],
+        ]
+        dr_set.to_update_element()
+        dr_set.add_dependent_element(1, dependent_element_params, as_group=True)
+        dr_set.save_element()
+        dr_set.to_update_element()
+        self.assertTrue(dr_set.element_have_dependent(dependent_element_params))
+        self.assertTrue(dr_set.element_have_params(params_list))
+        dr_set.delete_element()
+        self.assertFalse(dr_set.have_element())
+
+    def test07_update_second_product_with_dependent_element(self):
+        params_list = [
+            ["version", ["Внутренняя"]],
+            ["region", ["СНГ"]],
+        ]
+        product_params = (1, 'RSD 01', ('Цех', 'Кладовщик', 'Мастер'))
+        dr_set = self._add_simple_driver_set_with_2_products(params_list, product_params)
+        dependent_element_params = [
+            ["fx", ["Количество", "1"]],
+            ["version", ["Дилерская"]],
+            ["region", ["Азия", "Европа"]],
+        ]
+        dr_set.to_update_element()
+        dr_set.add_dependent_element(1, dependent_element_params)
+        dr_set.save_element()
+        dr_set.to_update_element()
+        new_product_params = (1, 'Все изделия', ('Цех', 'Кладовщик'))
+        dr_set.update_second_product(new_product_params)
+        dr_set.save_element()
+        dr_set.to_update_element()
+        self.assertTrue(dr_set.element_have_product(new_product_params))
+        self.assertTrue(dr_set.element_have_dependent(dependent_element_params))
+        self.assertTrue(dr_set.element_have_params(params_list))
+        dr_set.delete_element()
+        self.assertFalse(dr_set.have_element())
+
+    # def test08_update_dependent_element(self):
+    #     raise NotImplementedError
+    # 
+    # def test09_remove_dependent_element(self):
+    #     raise NotImplementedError
+
+    def test10_remove_second_product(self):
+        params_list = [
+            ["version", ["Внутренняя"]],
+            ["region", ["СНГ"]],
+        ]
+        product_params = (1, 'RSD 01', ('Цех', 'Кладовщик', 'Мастер'))
+        dr_set = self._add_simple_driver_set_with_2_products(params_list, product_params)
+        dr_set.to_update_element()
+        dr_set.delete_second_product()
+        dr_set.to_update_element()
+        self.assertFalse(dr_set.element_have_product(product_params))
+        self.assertTrue(dr_set.element_have_params(params_list))
         dr_set.delete_element()
         self.assertFalse(dr_set.have_element())
